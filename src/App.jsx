755fc30b0dc439994ca27e6dc45e56c08c797d00
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-07-04 00:06:50 -0300

    Add a redirect to land at /login

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import { Route, Router } from "wouter"
+import { Route, Router, useLocation } from "wouter"
 import LoginPage from "./pages/LoginPage"
 import NotGaliciaMsgPage from "./pages/NotGaliciaMsgPage"
 import { UserContextProvider } from "./context/userContext"
 
-const App = () => (
-  <UserContextProvider>
-    <Router>
-      <Route path="/login" component={LoginPage} />
-      <Route path="/notgalicia" component={NotGaliciaMsgPage} />
-    </Router>
-  </UserContextProvider>
-)
+const App = () => {
+  const [location, setLocation] = useLocation()
+
+  return (
+    <UserContextProvider>
+      <Router>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/notgalicia" component={NotGaliciaMsgPage} />
+        <Route path="/" component={() => setLocation("login")} />
+      </Router>
+    </UserContextProvider>
+  )
+}
 
 export default App
